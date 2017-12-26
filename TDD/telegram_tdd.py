a1ab919b11a09242b3c216995c471e7f1b28f611
Mark Choi <admin@raimsoft.com>
2017-12-26 18:54:51 +0900

    Update telegram_tdd.py

--- TDD/telegram_tdd.py
+++ TDD/telegram_tdd.py
@@ -1,8 +1,8 @@
 # written in python 3.*
 #-*- coding: utf-8 -*-
 import telegram
-
-bot = telegram.Bot(token='000000000:AAGjKiBi8bZzB4wL8wIWvQSDG5wmwlxmOnc')
+import Config
+bot = telegram.Bot(token=Config.TELEGRAM_CLIENT_TOKEN)
 
 chat_id = bot.getUpdates()[-1].message.chat.id
 
